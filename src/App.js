7a86e073427273af599eae2eb5af868d72acf13f
Nathan Stringer <nathstringer@gmail.com>
2023-11-06 11:23:51 +0000

    city and country inputs working with seperate state

--- src/App.js
+++ src/App.js
@@ -2,19 +2,22 @@ import { React, useState, useEffect } from "react";
 import { getWeatherData } from "./utils";
 import "./App.css";
 import Current from "./comps/current/Current";
+import LocationInput from "./comps/locationInput/LocationInput";
 
 function App() {
   const [currentWeather, setCurrentWeather] = useState([]);
+  const [location, setLocation] = useState("London");
   useEffect(() => {
     const fetchCurrentData = async () => {
-      const response = await getWeatherData();
+      const response = await getWeatherData(location);
       setCurrentWeather([response]);
     };
     fetchCurrentData();
-  }, []);
+  }, [location]);
   console.log(currentWeather);
   return (
     <div className="App">
+      <LocationInput location={location} setLocation={setLocation} />
       <Current currentWeather={currentWeather} />
     </div>
   );
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,9 +1,9 @@
 const apiKey = process.env.REACT_APP_API_KEY;
 
-export const getWeatherData = async () => {
+export const getWeatherData = async (location) => {
   try {
     const response = await fetch(
-      `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=London`,
+      `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location}`,
       {
         method: "GET",
         mode: "cors",
